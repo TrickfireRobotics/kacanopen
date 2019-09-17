f6c675d7aced8fad3a10f1b2f8e8ef383967ef91
Daniel Mamaghani <daniel.mamaghani@gmail.com>
2019-09-17 14:04:38 -0700

    Enable 402 option "Change on Setpoint" and disable option "Change Setpoint Immediately".

--- master/src/profiles.cpp
+++ master/src/profiles.cpp
@@ -74,8 +74,7 @@ namespace kaco {
 						device.set_entry("Target position", target_position);
                         device.execute("set_controlword_flag","controlword_pp_new_set_point");
 						device.execute("unset_controlword_flag","controlword_pp_new_set_point");
-						device.execute("set_controlword_flag","controlword_pp_change_set_immediately");
-                        //device.execute("unset_controlword_flag","controlword_pp_change_set_immediately");
+						device.execute("set_controlword_flag","controlword_pp_change_on_set_point");
 						return Value();
 					}
 				}
@@ -119,7 +118,7 @@ namespace kaco {
 				{ "controlword_pp_new_set_point",               static_cast<uint16_t>(1<<4) },
 				{ "controlword_pp_change_set_immediately",      static_cast<uint16_t>(1<<5) },
 				{ "controlword_pp_abs_rel",                     static_cast<uint16_t>(1<<6) },
-                { "controlword_pp_change_on_setpoint",          static_cast<uint16_t>(1<<9) },
+                { "controlword_pp_change_on_set_point",          static_cast<uint16_t>(1<<9) },
 				
 				// control word flags homing mode specific
 				{ "controlword_hm_operation_start",	static_cast<uint16_t>(1<<4) },
